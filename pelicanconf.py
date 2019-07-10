f6603e95cca4f96adfa1de69051bf985d96de9db
maggick <keller.mdpa@gmail.com>
2019-07-10 15:41:53 +0200

    Remove useless links and social

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'maggick'
-SITENAME = 'Recette'
+SITENAME = 'Recettes d\'arrière grand-mère'
 SITEURL = ''
 
 PATH = 'content'
@@ -21,13 +21,11 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python.org', 'http://python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
