12a9fec684ade224cd73f2edcd91e454f97f273c
maggick <maggick@users.noreply.github.com>
2023-03-11 09:50:44 +0100

    Clean pelican things

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,14 +12,10 @@ RELATIVE_ULRS = True
 TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = 'fr'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 DEFAULT_PAGINATION = 10
 
 STATIC_PATHS = ['extra/.nojekyll']
 EXTRA_PATH_METADATA = {'extra/.nojekyll':{'path':'.nojekyll'},}
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
